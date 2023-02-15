f59ba32853fac4e8852206b5a999fc8d69efa1e9
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-02-16 00:57:06 +0200

    -

--- listcomprehension/random.py
+++ listcomprehension/random.py
@@ -5,3 +5,13 @@ print(new)
 
 # reversing a string
 print("Hello"[::-1])
+
+# list(map(int, string_list)) is the same as [int(el) for el in string_list]
+# list(map(lambda x: x*2, numbers_list)) is the same as [el * 2 for el in numbers_list]
+
+# list(filter(lambda x: x % 2 == 0, numbers_list)) is the same as
+# [el for el in numbers_list if el % 2 == 0]
+
+numbers = list(map(int, input().split(", ")))
+print(list(filter(lambda x: x % 2 == 0, numbers)))
+# prints all even numbers
